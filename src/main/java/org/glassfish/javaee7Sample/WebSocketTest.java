241642bb029a13fce8d6659d25f1a5ca887a1871
Ignacio Herrero <herrero.olivari@gmail.com>
2013-10-08 12:59:06 -0300

    Se crearon diferentes endpoints para las versiones multiusuario y simple. se agrego un home

--- src/main/java/org/glassfish/javaee7Sample/WebSocketTest.java
+++ src/main/java/org/glassfish/javaee7Sample/WebSocketTest.java
@@ -14,33 +14,37 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/websocket")
 public class WebSocketTest {
 
-	private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
-	
 	@OnMessage
     public void onMessage(String message, Session session) 
-    	throws IOException {
-		
-		synchronized(clients){
-			// Iterate over the connected sessions
-			// and broadcast the received message
-			for(Session client : clients){
-				if (!client.equals(session)){
-					client.getBasicRemote().sendText(message);
-				}
-			}
+    	throws IOException, InterruptedException {
+
+		// Print the client message for testing purposes
+		System.out.println("Received: " + message);
+
+		// Send the first message to the client
+		session.getBasicRemote().sendText("This is the first server message");
+
+		// Send 3 messages to the client every 5 seconds
+		int sentMessages = 0;
+		while(sentMessages < 3){
+			Thread.sleep(5000);
+			session.getBasicRemote().
+				sendText("This is an intermediate server message. Count: " 
+					+ sentMessages);
+			sentMessages++;
 		}
-		
+
+		// Send a final message to the client
+		session.getBasicRemote().sendText("This is the last server message");
     }
-	
+
 	@OnOpen
-    public void onOpen (Session session) {
-		// Add session to the connected sessions set
-		clients.add(session);
+    public void onOpen () {
+        System.out.println("Client connected");
     }
 
     @OnClose
-    public void onClose (Session session) {
-    	// Remove session from the connected sessions set
-    	clients.remove(session);
+    public void onClose () {
+    	System.out.println("Connection closed");
     }
 }
